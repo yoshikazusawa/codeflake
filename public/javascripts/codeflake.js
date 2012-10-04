438ffc6218065ca4485612e513396bdda53bff46
Yoshikazu Sawa <yoshikazusawa@github.com>
2012-10-04 19:33:54 +0900

    remove codemirror

--- public/javascripts/codeflake.js
+++ public/javascripts/codeflake.js
@@ -1,13 +1,4 @@
 $(function() {
-    var codeMirror = CodeMirror.fromTextArea($('#code')[0], {
-        mode: $('.nav-tabs .active').text(),
-        theme: 'default',
-        lineNumbers: true,
-        smartIndent: true,
-        indentUnit : 4,
-        lineWrapping: true,
-        onChange: function() { codeMirror.save(); }
-    });
     var tabs = $('.nav-tabs li');
     var syntaxMode = $('form .syntax');
     tabs.click(function(evt) {
@@ -20,7 +11,6 @@ $(function() {
         tabs.removeClass('active');
         current.addClass('active');
 
-        codeMirror.setOption('mode', syntax);
         syntaxMode.val(syntax);
     });
 });
